2d9b3f5d2f6e50558626680db811901ed49fc32e
sndpokhrel <sandypokhrel09@gmail.com>
2025-01-07 10:57:31 +0545

    Added validation middleware for validating section names

---
+++ app/Http/Middleware/ValidateSectionName.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateSectionName
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $section = $request->route('section');
+
+        if (!preg_match('/^[a-z-]+$/', $section)) {
+            return response()->json(['error' => 'Invalid section name'], 400);
+        }
+        return $next($request);
+    }
+}
